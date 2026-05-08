6f5842a9e8a42b6d7d1429569cecff54df49690d
muhamadayeshaaulia <shaxyyy.03@gmail.com>
2026-05-08 14:25:48 +0700

    add : mainactifity kotlin
    menggunakan flutter fragmant actifity bukan flutter actifity

--- android/app/src/main/kotlin/com/example/biometrik_learn/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/biometrik_learn/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.biometrik_learn
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity : FlutterFragmentActivity()
